85607c4e4817f3fbde4008784171e5730d193a65
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2025-02-22 22:14:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ Created on Fri Feb 21 21:05:45 2025
 @author: tlee
 """
 
-from birddb.database import BirdDataBase
+from birddb.database import getBirdDataBase
+from birddb.plotting import plot_order, plot_family, plot_most_photographed
 import polars as pl
-#db = BirdDataBase(datafolder='D://Wildlife Photos/Birds/Classification')
-#pl.Config.set_tbl_rows(5)
 
-db = BirdDataBase()
-db.add_bulk_to_database('D:\Wildlife Photos\Birds\Classification')
+db = getBirdDataBase('D:/Wildlife Photos/Birds/Classification',force_overwrite=True)
+
+db.add_unsorted_to_database(ebird='https://ebird.org/checklist/S210041434')
+db.sort()
+db.save()
 
 print(db.df)
+
+#plot_order(db,mode='photos')
+#plot_order(db,mode='species')
+#plot_family(db,'Pelecaniformes',mode='photos')
+#plot_most_photographed(db)
+
+
