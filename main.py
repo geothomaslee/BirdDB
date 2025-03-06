5911e562a28a5a0ad88a0fc382f2240ab562898d
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2025-03-05 22:51:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,37 @@ Created on Fri Feb 21 21:05:45 2025
 @author: tlee
 """
 from birddb.database import getBirdDataBase
-from birddb.plotting import plot_orders, plot_families, plot_genuses, plot_most_photographed
-from birddb.plotting import plot_order_comp, plotPhyloTree
 import polars as pl
 
-import matplotlib.pyplot as plt
-
-import os
-
 db = getBirdDataBase('D:/Wildlife Photos/Birds/Classification',force_overwrite=False)
-#db.add_unsorted_to_database('https://ebird.org/checklist/S216014746')
-#db.sort()
-#db.save()
-
-#print(db.df)
+db.sort_new('https://ebird.org/checklist/S216728975')
 
 
-#plot_orders(db,mode='photos')
-#plot_orders(db,mode='species')
-#plot_families(db,'Passeriformes',mode='photos')
-#plot_genuses(db,'Anatidae',mode='photos')
-plot_most_photographed(db,30)
-#plot_order_comp(db,'Pelecaniformes',mode='photos')
-#plotPhyloTree(db,mode='species',tree_style=2)
+
+
+
+qu = db.search('Tufted titmouse')
+qu.show_images()
+
+
+
+
+
+#db.sort_new('https://ebird.org/checklist/S216056879')
+
+#=====================Bird DB planned features list============================
+# Fix bug with genus
+# Photography taken over time
+#   Stacked bar chart of orders would be cool
+# Ability to actually display photos
+# Query functions so you can search for species without having to known how DataFrames work
+#   Strings as single queries
+#   Dictionary where key=column and value=value, and return all matching values?
+#   Ability to display photos that are queried, or move them to a directory for better viewing ability
+# Get much clearer list of actually required libraries
+
+
+
 
 
 
