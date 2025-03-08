531d28a363a9052b8493e570c80de7847e02d3c5
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2025-03-08 09:39:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,7 @@ import polars as pl
 db = getBirdDataBase('D:/Wildlife Photos/Birds/Classification',force_overwrite=False)
 #db.sort_new('https://ebird.org/checklist/S216728975')
 
-from birddb.plotting import plot_orders_over_time
-plot_orders_over_time(db)
-
-
-
-#db.sort_new('https://ebird.org/checklist/S216056879')
-
 #=====================Bird DB planned features list============================
-# Photography taken over time
-#   Stacked bar chart of orders would be cool
 # Get much clearer list of actually required libraries
 
 
